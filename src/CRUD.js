1254968be4692490370187076a2992066701f516
MohsinAli-Developer <mohsinadspec@gmail.com>
2025-07-23 13:51:14 +0800

    Fix casing issues and connect API

--- src/CRUD.js
+++ src/CRUD.js
@@ -1,241 +1,3 @@
-// import React, { useState, useEffect, Fragment } from 'react';
-// import Table from 'react-bootstrap/Table';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import Button from 'react-bootstrap/Button';
-// import Modal from 'react-bootstrap/Modal';
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
-// import Container from 'react-bootstrap/Container';
-// import axios from 'axios';
-// import { toast, ToastContainer } from 'react-toastify'; 
-// import 'react-toastify/dist/ReactToastify.css'
-
-// const CRUD = () => {
-//   const [show, setShow] = useState(false);
-
-//   const handleClose = () => setShow(false);
-//   const handleShow = () => setShow(true);
-
-//   const [name, setName] = useState('');
-//     const [age, setAge] = useState('');
-//       const [isActive, setIsActive] = useState(0);
-        
-//         const [editID, setEditId] = useState('');
-//           const [editName, setEditName] = useState('');
-//             const [editAge, setEditAge] = useState('');
-//                 const [editIsActive, setEditIsActive] = useState(0);
-
-//   const empdata = [
-//     { id: 1, name: 'Mohsin', age: 22, isActive: 1 },
-//     { id: 2, name: 'Ali', age: 25, isActive: 0 },
-//   ];
-
-//   const [data, setData] = useState([]);
-
-//   useEffect(() => {
-//     getData();
-//   }, []);
-
-//   const getData = () =>{
-//     axios.get('https://localhost:7062/api/Employee')
-//     .then((result)=>{
-//       setData(result.data)
-//     })
-//     .catch((error)=>{
-//       console.log(error)
-//     })
-//   }
-
-//   const handleEdit = (id) =>{
-//     //alert (id);
-//     handleShow();
-//     axios.get(`https://localhost:7062/api/Employee/${id}`)
-//     .then((result)=>{
-//       setEditName(result.data.name);
-//             setEditAge(result.data.age);
-//                   setEditIsActive(result.data.isActive);
-//                         setEditId(id);
-//     })
-//     .catch((error)=>{
-//       console.log(error)
-//     })
-//   }
-
-//     const handleDelete = (id) =>{
-//       if(window.confirm("Are you sure to delete this employee") == true){
-//         axios.delete(`https://localhost:7062/api/Employee/${id}`)
-//           .then((result)=>{
-//             if(result.status === 200){
-//               toast.success('Employee has been deleted');
-//               getData();
-//             }
-//           })
-//           .catch((error)=>{
-//       toast.error(error);
-//         })
-//       }
-//   }
-
-//   const handleUpdate = () => {
-//     const url = `https://localhost:7062/api/Employee/${editID}`;
-//     const data = {
-//       "id": editID,
-//       "name": editName,
-//   "age": editAge,
-//   "isActive": editIsActive
-//     }
-
-//   axios.put(url, data)
-//     .then((result) =>{
-//       handleClose();
-//       getData();
-//       clear();
-//       toast.success('Employee has been updated');
-//     })
-//     .catch((error)=>{
-//       toast.success(error);
-//     })
-//   }
-
-//   const handleSave =() =>{
-//     const url = 'https://localhost:7062/api/Employee';
-//     const data = {
-//       "name": name,
-//   "age": age,
-//   "isActive": isActive
-//     }
-
-//     axios.post(url, data)
-//     .then((result) =>{
-//       getData();
-//       clear();
-//       toast.success('Employee has been added');
-//     })
-//     .catch((error)=>{
-//       toast.success(error);
-//     })
-//   }
-
-//   const clear = () =>{
-//     setName('');
-//     setAge('');
-//     setIsActive(0);
-//     setEditName('');
-//     setEditAge('');
-//     setEditIsActive(0);
-//     setEditId('');
-//   }
-
-//   const handleActiveChange =(e) =>{
-//     if(e.target.checked){
-//       setIsActive(1);
-//     }
-//     else{
-//       setIsActive(0);
-//     }
-//   }
-
-//    const handleEditActiveChange =(e) =>{
-//     if(e.target.checked){
-//       setEditIsActive(1);
-//     }
-//     else{
-//       setEditIsActive(0);
-//     }
-//   }
-//   return (
-//     <Fragment>
-//       <ToastContainer />
-//       <Container>
-//       <Row>
-//         <Col>
-//         <input type="text" className='form-control' placeholder='Enter Name'
-//         value={name} onChange={(e)=> setName(e.target.value)}/>
-//         </Col>
-//         <Col>
-//         <input type="text" className='form-control' placeholder='Enter Age'
-//         value={age} onChange={(e)=> setAge(e.target.value)}/>
-//         </Col>
-//         <Col>
-//         <input type="checkbox" 
-//         checked={isActive === 1 ? true : false}
-//         onChange={(e) => handleActiveChange(e)}value={isActive}/>
-//         <label >IsActive</label>
-//         </Col>
-//         <Col>
-//         <button className="btn btn-primary" onClick={()=> handleSave()}>Submit</button>
-//         </Col>
-//       </Row>
-//     </Container>
-//     <br></br>
-//       <Table striped bordered hover>
-//         <thead>
-//           <tr>
-//             <th>#</th>
-//             <th>Name</th>
-//             <th>Age</th>
-//             <th>IsActive</th>
-//               <th>Action</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {data && data.length > 0 ? (
-//             data.map((item, index) => (
-//               <tr key={index}>
-//                 <td>{index + 1}</td>
-//                 <td>{item.name}</td>
-//                 <td>{item.age}</td>
-//                 <td>{item.isActive ? 'Yes' : 'No'}</td>
-//                 <td colSpan={2}>
-//                   <button className="btn btn-primary" onClick={()=> handleEdit(item.id)}>Edit</button> &nbsp;
-//                   <button className="btn btn-danger" onClick={()=> handleDelete(item.id)}>Delete</button>                  
-//                 </td>
-//               </tr>
-//             ))
-//           ) : (
-//             <tr>
-//               <td colSpan="5">Loading...</td>
-//             </tr>
-//           )}
-//         </tbody>
-//       </Table>
-
-//       <Modal show={show} onHide={handleClose}>
-//         <Modal.Header closeButton>
-//           <Modal.Title>Modify / Update Employee</Modal.Title>
-//         </Modal.Header>
-//         <Modal.Body>
-//           <Row>
-//         <Col>
-//         <input type="text" className='form-control' placeholder='Enter Name'
-//         value={editName} onChange={(e)=> setEditName(e.target.value)}/>
-//         </Col>
-//         <Col>
-//         <input type="text" className='form-control' placeholder='Enter Age'
-//         value={editAge} onChange={(e)=> setEditAge(e.target.value)}/>
-//         </Col>
-//         <Col>
-//         <input type="checkbox" 
-//         checked={editIsActive === 1 ? true : false}
-//         onChange={(e) => handleEditActiveChange(e)}value={editIsActive}/>
-//         <label >IsActive</label>
-//         </Col>
-//       </Row>
-//         </Modal.Body>
-//         <Modal.Footer>
-//           <Button variant="secondary" onClick={handleClose}>
-//             Close
-//           </Button>
-//           <Button variant="primary" onClick={handleUpdate}>
-//             Save Changes
-//           </Button>
-//         </Modal.Footer>
-//       </Modal>
-//     </Fragment>
-//   );
-// };
-// export default CRUD;
-
 import React, { useState, useEffect, Fragment } from 'react';
 import { Table, Button, Modal, Row, Col, Container, Card, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -298,11 +60,12 @@ const CRUD = () => {
   const handleUpdate = () => {
     const url = `https://infoapplication-afewfch2hye4htew.canadacentral-01.azurewebsites.net/api/Employee/${editID}`;
     const data = {
-  ID: editID,
-  Name: editName,
-  Age: editAge,
-  IsActive: editIsActive
+  id: editID,
+  name: editName,
+  age: editAge,
+  isActive: editIsActive
 };
+
 
 
     axios.put(url, data)
@@ -318,9 +81,9 @@ const CRUD = () => {
   const handleSave = () => {
     const url = 'https://infoapplication-afewfch2hye4htew.canadacentral-01.azurewebsites.net/api/Employee';
     const data = {
-  Name: name,
-  Age: age,
-  IsActive: isActive
+  name: name,
+  age: age,
+  isActive: isActive
 };
 
 
@@ -391,14 +154,14 @@ const CRUD = () => {
                 data.map((item, index) => (
                   <tr key={index}>
                     <td>{index + 1}</td>
-                    <td>{item.Name}</td>
-<td>{item.Age}</td>
-<td>{item.IsActive ? 'Yes' : 'No'}</td>
+                    <td>{item.name}</td>
+<td>{item.age}</td>
+<td>{item.isActive ? 'Yes' : 'No'}</td>
                     <td>
-                      <Button variant="primary" size="sm" onClick={() => handleEdit(item.ID)}>
+                      <Button variant="primary" size="sm" onClick={() => handleEdit(item.id)}>
                         <i className="bi bi-pencil-square"></i>
                       </Button>{' '}
-                      <Button variant="danger" size="sm" onClick={() => handleDelete(item.ID)}>
+                      <Button variant="danger" size="sm" onClick={() => handleDelete(item.id)}>
                         <i className="bi bi-trash"></i>
                       </Button>
                     </td>
